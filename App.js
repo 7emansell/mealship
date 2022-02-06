b00aeccbc7ced470cdc5984084638380a9098df4
Emma Mansell <emilyjmansell@gmail.com>
2022-02-06 03:02:51 +0100

    recipient screen

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
@@ -9,6 +8,64 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function DonorMapScreen({ navigation }){
   const DonorMapComponent = () => {
+      const [location, setLocation] = React.useState(null)
+      const [error, setError] = React.useState(null)
+
+      React.useEffect(() => {
+          (async () =>{
+              let { status } = await Location.requestPermissionsAsync();
+              if(status !== 'granted'){
+                  setError('Permission to access location was denied');
+                  return;
+              }
+              const locate = await Location.getCurrentPositionAsync({});
+              setLocation(locate.coords)
+          })()
+      }, []);
+
+      return (
+          <View>
+              <Text style={styles.heading}>Mealship</Text>
+              <MapView style={styles.map}>
+                  {location ? (
+                      <Marker coordinate={location} title="your location" >
+                          <FontAwesome name="map-marker" size={40} color="#B12A5B" />
+                      </Marker>
+                  ):
+                      <Text>{error}</Text>
+                  }
+                <MapView.Circle
+                  center = { location }
+                  radius = { 1000 }
+                  strokeWidth = { 1 }
+                  strokeColor = { '#1a66ff' }
+                  fillColor = { 'rgba(230,238,255,0.5)' }
+                />
+                <Marker coordinate={location} title="John" >
+                          <FontAwesome name="map-pin" size={40} color="#454B1B" />
+                </Marker>
+              </MapView>
+          </View>
+      );
+  };
+
+  const styles = StyleSheet.create({
+      map: {
+          width: Dimensions.get('screen').width,
+          height: Dimensions.get('screen').height * 0.90,
+      },
+      heading: {
+          alignSelf: 'center',
+          paddingTop: 20,
+          marginBottom: 10,
+          fontSize: 24
+      },
+  });
+}
+
+
+function RecipientMapScreen({ navigation }){
+  const RecipientMapComponent = () => {
       const [location, setLocation] = React.useState(null)
       const [error, setError] = React.useState(null)
 
@@ -84,6 +141,7 @@ function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Donor Home" component={DonorMapScreen} />
+        <Stack.Screen name="Recipient Home" component={RecipientMapScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
